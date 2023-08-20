4a1776dba7e66246eca9058345b57e61e714a6cb
Thiago Maceda <thiagopmaceda@gmail.com>
2023-08-19 22:50:29 -0300

    adjust schema tests
    Signed-off-by: Thiago Maceda <thiagopmaceda@gmail.com>

---
+++ test/cumbuca/accounts/login_test.exs
@@ -0,0 +1,24 @@
+defmodule Cumbuca.Accounts.LoginTest do
+  use Cumbuca.DataCase
+
+  alias Cumbuca.Accounts.Login
+
+  describe "changeset/2" do
+    test "with valid data returns valid changeset" do
+      attrs = %{cpf: "34645544063", password: "a1b4$%2b"}
+
+      changeset = Login.changeset(%Login{}, attrs)
+
+      assert changeset.valid?
+      assert changeset.changes == attrs
+    end
+
+    test "with invalid data returns error changeset" do
+      changeset = Login.changeset(%Login{}, %{})
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{cpf: ["can't be blank"], password: ["can't be blank"]}
+    end
+  end
+end
--- test/cumbuca/accounts_test.exs
+++ test/cumbuca/accounts_test.exs
@@ -4,34 +4,20 @@ defmodule Cumbuca.AccountsTest do
   alias Cumbuca.Accounts
   alias Cumbuca.Accounts.Account
 
-  describe "accounts" do
-    test "create_account/1 with valid data creates a account" do
+  describe "create_account/1" do
+    test "with valid data creates a account" do
       user = insert!(:user)
       attrs = %{balance: 42_000, user_id: user.id}
 
       assert {:ok, %Account{} = account} = Accounts.create_account(attrs)
-      assert account.balance == 42_000
-      assert account.user_id == user.id
+
+      assert Repo.get_by(id: account.id, balance: account.balance, user_id: account.user_id)
     end
 
-    test "create_account/1 with invalid data returns error changeset" do
+    test "with invalid data returns error changeset" do
       {:error, changeset} = Accounts.create_account(%{})
 
       assert errors_on(changeset) == %{balance: ["can't be blank"], user_id: ["can't be blank"]}
-    end
-
-    test "create_account/1 handles foreign key constraint" do
-      user_id = Ecto.UUID.generate()
-      {:error, changeset} = Accounts.create_account(%{balance: 200, user_id: user_id})
-
-      assert errors_on(changeset) == %{user_id: ["does not exist"]}
-    end
-
-    test "create_account/1 with negative initial balance returns error changeset" do
-      user_id = Ecto.UUID.generate()
-      {:error, changeset} = Accounts.create_account(%{balance: -30_000, user_id: user_id})
-
-      assert errors_on(changeset) == %{balance: ["must be greater than 0"]}
     end
   end
 end
